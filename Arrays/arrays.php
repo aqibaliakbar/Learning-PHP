332100e062e357990c9f49e732a19fa49c1053cd
aqibaliakbar <aqibaliakbar@gmail.com>
2024-02-12 19:13:33 +0500

    How to display Associative Arrays!

--- Arrays/arrays.php
+++ Arrays/arrays.php
@@ -42,37 +42,57 @@
 		[
 			'name' => 'Atomic Habits',
 			'author' => 'John Doe',
+			'releaseYear' => 1995,
 			'url' => 'https://www.amazon.com/Atomic-Habits',
 		],
 		[
 			'name' => 'Deeniyat',
 			'author' => 'Molana Mududi',
+			'releaseYear' => 1995,
 			'url' => 'https://www.amazon.com/Atomic-Habits',
 		],
 		[
 			'name' => 'Psychology of Men',
 			'author' => 'Molana Mududi',
+			'releaseYear' => 1995,
 			'url' => 'https://www.amazon.com/Atomic-Habits',
 		]
 	];
 	?>
 
+	<!--To display all keys of the first array in $bookCollection, you can access the keys using the array_keys() function and then loop through them to display them. -->
+	<ul>
+		<?php $firstBookKeys = array_keys($bookCollection[0]); ?>
+		<?php foreach ($firstBookKeys as $key) : ?>
+			<li><?= $key ?></li>
+		<?php endforeach; ?>
+	</ul>
+
+	<!--If you want to display both the keys and their corresponding values for the first array in $bookCollection, you can use a foreach loop to iterate over the key-value pairs.--->
+	<ul>
+		<?php foreach($bookCollection[0] as $key => $value) : ?>
+			<li><?= $key ?> : <?= $value?></li>
+			<?php endforeach;?>
+	</ul>
+
 	<ul>
 		<?php foreach ($bookCollection as $book) : ?>
-			<li><?php echo $book['name'] ?></li>
+			<a href="<?= $book["url"]; ?>">
+				<li><?php echo $book['name'] ?> (<?php echo $book['releaseYear'] ?> ) </li>
+			</a>
 		<?php endforeach; ?>
 	</ul>
 
 	<!-- Associative  arrays in php - only accessing 3rd array from associative array by looping -->
 
 	<ul>
-		<?php $count = 0;?>
+		<?php $count = 0; ?>
 		<?php foreach ($bookCollection as $book) : ?>
-		<?php if ($count == 2) : ?>
-			<li><?php echo $book['name'] ?></li>
+			<?php if ($count == 2) : ?>
+				<li><?php echo $book['name'] ?></li>
 			<?php endif; ?>
-			<?php $count++;?>
-			<?php endforeach; ?>
+			<?php $count++; ?>
+		<?php endforeach; ?>
 	</ul>
 
 
